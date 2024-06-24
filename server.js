7183828392c8738e61c4d8670c96ab0b885b0982
Abhay Gupta <111756624+thisabhay@users.noreply.github.com>
2024-06-24 07:57:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static('public'));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
